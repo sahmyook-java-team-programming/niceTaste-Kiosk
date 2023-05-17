bfbfd62b0f31a076ab3327b7b130b21c53dadaf9
choivember <pocketmongyo@gmail.com>
2023-05-17 13:13:52 +0900

    Produce 추가

---
+++ src/main/java/kr/ac/sahmyook/nicetaste/kiosk/func/Produce.java
@@ -0,0 +1,18 @@
+package kr.ac.sahmyook.nicetaste.kiosk.func;
+
+    public class Produce {
+
+        public void makeAmericano() {
+            System.out.println("에스프레소를 내립니다.");
+            System.out.println("물을 넣습니다.");
+        }
+        public void makeCafeLatte() {
+            System.out.println("에스프레소를 내립니다.");
+            System.out.println("우유를 넣습니다.");
+        }
+        public void makeVanillaLatte() {
+            System.out.println("에스프레소를 내립니다.");
+            System.out.println("우유를 넣습니다.");
+            System.out.println("바닐라 시럽을 넣습니다.");
+        }
+    }
